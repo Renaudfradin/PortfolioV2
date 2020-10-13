fc5f5e654f202390d81c816da2028ade5df45503
Renaud <renaudfradin@gmail.com>
2020-10-13 19:15:34 +0200

    works

--- src/components/Works/Works.vue
+++ src/components/Works/Works.vue
@@ -1,233 +1,24 @@
 <template>
     <div class="home">
       <div class="row">
-        <div class="col">
+        <div v-for="(projet,index) in tableproj" v-bind:key="index" class="col" >
           <div class="style-card1">
             <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureappvue.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Applications-Météo</h5>
-                <p class="card-text">Applications-meteo realiser avec BOOTSTRAP et VUE.js et l'API OpenWeather</p>
-                <p class="card-text">Septembre 2020</p>
+              <img class="card-img-top img-projet" :src="projet.imgProjet" alt="">
+              <div class="cardbody">
+                <div class="text-info">
+                  <h5 class="card-title">{{projet.nomProjet}}</h5>
+                  <p class="card-text">{{projet.diescriptionProjet}}</p>
+                  <p class="card-text">{{projet.realisationProjet}}</p>
+                </div>
                 <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="https://github.com/Renaudfradin/app-meteo" class="btn btn-primary btn-info">code</a>
+                  <a :href="projet.urlSiteProjet" class="btn btn-primary btn-info">site</a>
+                  <a :href="projet.urlCodeProjet" class="btn btn-primary btn-info">code</a>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureappvue.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">Applications-Météo</h5>
-                <p class="card-text">Applications-meteo realiser avec HTML/CSS/JS et l'API prevision-meteo.ch</p>
-                <p class="card-text">Septembre 2020</p>
-                <div class="btn-parent">
-                  <a href="https://renaudfradin.github.io/aplie-meteo/" class="btn btn-primary btn-info">site</a>
-                  <a href="https://github.com/Renaudfradin/aplie-meteo" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Capturetodo.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">TODO-LIST</h5>
-                <p class="card-text">TODO-LIST realiser en HTML/CSS/JS</p><br>
-                <p class="card-text">Septembre 2020</p><br>
-                <div class="btn-parent">
-                  <a href="https://renaudfradin.github.io/TODO-LIST/" class="btn btn-primary btn-info">site</a>
-                  <a href="https://github.com/Renaudfradin/TODO-LIST" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureport.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">PORTFOLIO V1</h5>
-                <p class="card-text">Portfolio qui regroupe mes competance realiser en BULMA/JS</p>
-                <p class="card-text">Decembre 2019 - Septembre 2020 </p>
-                <div class="btn-parent">
-                  <a href="https://portfolio-renaud-fradin.yj.fr/index.html" class="btn btn-primary btn-info">site</a>
-                  <a href="https://github.com/Renaudfradin/Portfolio" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureportv2.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">PORTFOLIO V2</h5>
-                <p class="card-text">Portfolio qui regroupe mes competance realiser en HTML/CSS BOOTSTRAP et VUE.JS</p>
-                <p class="card-text">Octobre 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="#" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureappvue.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">PORTFOLIO V2</h5>
-                <p class="card-text">Portfolio qui regroupe mes competance realiser en HTML/CSS BOOTSTRAP et VUE.JS</p>
-                <p class="card-text">Octobre 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="#" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Capture.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">BOT DISCORD(MaXiMe)</h5>
-                <p class="card-text">BOT réalisé pour le logiciel <a style="color:#08fdd8;text-decoration: none;" href="https://discord.com/">Discord</a></p><br>
-                <p class="card-text">Mai 2020</p>
-                <div class="btn-parent">
-                  <!--<a href="#" class="btn btn-primary btn-info">site</a>-->
-                  <a href="https://github.com/Renaudfradin/BOT-MAXIME-DISCORD" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Screenshot_2020-04-21IntranetduLaboratoireGalaxy-SwissBourdin.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">GSB-PHP</h5>
-                <p class="card-text">Application de gestion de frais Réalisé en BULMA/PHP/SQL</p>
-                <p class="card-text">Mars 2020 - Juin 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="https://github.com/Renaudfradin/GSB-PHP" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/GSB-JAVA.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">GSB-JAVA</h5>
-                <p class="card-text">Application de gestion de compte-rendu de visite Réalisé en JAVA</p>
-                <p class="card-text">Février 2020 - Mai 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="#" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/EDITCALC.jpg" alt="">
-              <div class="card-body">
-                <h5 class="card-title">EDITCALC</h5>
-                <p class="card-text">Calculette pour enfant Réalisé en JAVA/SQL</p>
-                <p class="card-text">Novembre 2019 - Avril 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="#" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureappvue.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">EDITCALC</h5>
-                <p class="card-text">Calculette pour enfant Réalisé en JAVA/SQL</p>
-                <p class="card-text">Novembre 2019 - Avril 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="#" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureappvue.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">EDITCALC</h5>
-                <p class="card-text">Calculette pour enfant Réalisé en JAVA/SQL</p>
-                <p class="card-text">Novembre 2019 - Avril 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="#" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureappvue.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">EDITCALC</h5>
-                <p class="card-text">Calculette pour enfant Réalisé en JAVA/SQL</p>
-                <p class="card-text">Novembre 2019 - Avril 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="#" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="style-card1">
-            <div class="style-card">
-              <img class="card-img-top img-projet" src="../../assets/Captureappvue.png" alt="">
-              <div class="card-body">
-                <h5 class="card-title">EDITCALC</h5>
-                <p class="card-text">Calculette pour enfant Réalisé en JAVA/SQL</p>
-                <p class="card-text">Novembre 2019 - Avril 2020</p>
-                <div class="btn-parent">
-                  <a href="#" class="btn btn-primary btn-info">site</a>
-                  <a href="#" class="btn btn-primary btn-info">code</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        
+        </div> 
       </div>
     </div>
 </template>
@@ -236,122 +27,22 @@ export default {
     name:'Works',
     data(){
         return{
-          loup:"./assets/logo.png",
+          tableproj:[
+            {nomProjet:"PROTFOLIO V1",diescriptionProjet:"Portfolio qui regroupe mes competance realiser en BULMA/JS",realisationProjet:"Decembre 2019 - Septembre 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/Captureport.png",urlCodeProjet:"https://github.com/Renaudfradin/Portfolio",urlSiteProjet:"https://portfolio-renaud-fradin.yj.fr/index.html"},
+            {nomProjet:"PROTFOLIO V2",diescriptionProjet:"Portfolio qui regroupe mes competance realiser en HTML/CSS BOOTSTRAP et VUE.JS",realisationProjet:"Octobre 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/Captureportv2.png",urlCodeProjet:"https://github.com/Renaudfradin/PortfolioV2",urlSiteProjet:"https://github.com/Renaudfradin/PortfolioV2"},
+            {nomProjet:"Applications-Météo",diescriptionProjet:"Applications-meteo realiser avec BOOTSTRAP et VUE.js et l'API OpenWeather",realisationProjet:"Septembre 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/Captureappvue.png",urlCodeProjet:"https://github.com/Renaudfradin/app-meteo",urlSiteProjet:"https://github.com/Renaudfradin/app-meteo"},
+            {nomProjet:"Applications-Météo",diescriptionProjet:"Applications-meteo realiser avec HTML/CSS/JS et l'API prevision-meteo.ch",realisationProjet:"Septembre 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/Capturemeteo.png",urlCodeProjet:"https://github.com/Renaudfradin/aplie-meteo",urlSiteProjet:"https://renaudfradin.github.io/aplie-meteo/"},
+            {nomProjet:"TODO-LIST",diescriptionProjet:"TODO-LIST realiser en HTML/CSS/JS",realisationProjet:"Septembre 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/Capturetodo.png",urlCodeProjet:"https://github.com/Renaudfradin/TODO-LIST",urlSiteProjet:"https://renaudfradin.github.io/TODO-LIST/"},
+            {nomProjet:"The Skateshop",diescriptionProjet:"Site vitrine de vete de skate realiser en HTML/CSS/JS",realisationProjet:"Avril 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/TheSkateshop.png",urlCodeProjet:"https://github.com/Renaudfradin/The-Skateshop",urlSiteProjet:"hhttps://renaudfradin.github.io/The-Skateshop/"},
+            {nomProjet:"BOT DISCORD(MaXiMe)",diescriptionProjet:"BOT réalisé pour le logiciel Discord",realisationProjet:"Mai 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/Capture.png",urlCodeProjet:"https://github.com/Renaudfradin/BOT-MAXIME-DISCORD",urlSiteProjet:"https://github.com/Renaudfradin/BOT-MAXIME-DISCORD"},
+            {nomProjet:"GSB-PHP",diescriptionProjet:"Application de gestion de frais Réalisé en BULMA/PHP/SQL",realisationProjet:"Mars 2020 - Juin 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/Screenshot_2020-04-21IntranetduLaboratoireGalaxy-SwissBourdin.png",urlCodeProjet:"https://github.com/Renaudfradin/GSB-PHP",urlSiteProjet:"https://github.com/Renaudfradin/GSB-PHP"},
+            {nomProjet:"GSB-JAVA",diescriptionProjet:"Application de gestion de compte-rendu de visite Réalisé en JAVA",realisationProjet:"Février 2020 - Mai 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/GSB-JAVA.png",urlCodeProjet:"https://github.com/Renaudfradin/GSB-JAVA",urlSiteProjet:"https://github.com/Renaudfradin/GSB-JAVA"},
+            {nomProjet:"EDITCALC",diescriptionProjet:"Calculette pour enfant Réalisé en JAVA/SQL",realisationProjet:"Novembre 2019 - Avril 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/EDITCALC.jpg",urlCodeProjet:"https://github.com/Renaudfradin/EDITCAL-JAVA",urlSiteProjet:"https://github.com/Renaudfradin/EDITCAL-JAVA"},
+            {nomProjet:"Location-Cars-V1",diescriptionProjet:"Site de locations de voitures (ﬁctif)Réalisé en HTML/CSS/JS/PHP/SQL",realisationProjet:"Avril 2019 - Juin 2019",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/locationscarsv12.png",urlCodeProjet:"https://github.com/Renaudfradin/location-car-v1/tree/master/location-cars",urlSiteProjet:"https://github.com/Renaudfradin/location-car-v1/tree/master/location-cars"},
+            {nomProjet:"Location-Cars-V2",diescriptionProjet:"Front du nouveau Site de locations de voitures (ﬁctif)Réalisé en BULMA/JS",realisationProjet:"Février 2020",imgProjet:"https://raw.githubusercontent.com/Renaudfradin/PortfolioV2/master/src/assets/locationscarsv12.png",urlCodeProjet:"https://github.com/Renaudfradin/location-cars-V2",urlSiteProjet:"https://renaudfradin.github.io/location-cars-V2/"},
+          ]
         }
     }
 }
 </script>
-<style scoped>
-.home{
-  width: 100%;
-  height: calc(100vh - 40px);
-  color: white;
-  margin-left: 5%;
-  margin-right: 0%;
-}
-.row{
-  margin-right:0%;
-  margin-left:0%;
-}
-.col{
-  margin-top:20px;
-  flex-grow: 0;
-}
-.style-card{
-  color: #08fdd8;
-  border: 1px solid #181818;
-  background: #222;
-  width: 250px;
-  height: 100%;
-  margin: 0%;
-  padding: 0%;
-}
-.card-text{
-  color: white;
-}
-.btn-parent{
-  display: flex;
-  justify-content: center;
-}
-.btn-info{
-  border: 1px solid #08fdd8;
-  background-color: #2b2b2b;
-  color: #08fdd8;
-  text-align: center;
-  font-size: 15px;
-  text-decoration: none;
-  letter-spacing: 3px;
-  padding: 6px 12px;
-  text-transform: uppercase;
-  width: 50%;
-  display: block;
-  margin-right: 5px;
-  
-}
-.btn-info:hover{
-  background-color: #08fdd8;
-  color: #2b2b2b;
-}
-.img-projet{
-  width: 100%;
-  height: 100%;
-}
-@media screen and (max-width:770px) {
-  .home{
-    width: 100%;
-    height: calc(100vh - 40px);
-    color: white;
-    margin-left: 1px;
-    margin-right: 1px;
-  }
-  .img-projet{
-    width: 100%;
-    height: 100%;
-  }
-  .row{
-    margin-right:0%;
-    margin-left:0%;
-  }
-  .col{
-    margin-top:20px;
-    flex-grow: 1;
-  }
-}
-@media screen and (max-width:450px) {
-  .img-projet{
-    width: 100%;
-    height: 100%;
-  }
-  .style-card{
-    color: #08fdd8;
-    border: 1px solid #181818;
-    background: #222;
-    width: 100%;
-    height: 100%;
-    margin: 0%;
-    padding: 0%;
-}
-  
-}
-@media screen and (max-width:325px) {
-  .style-card{
-    color: #08fdd8;
-    border: 1px solid #181818;
-    background: #222;
-    width: 100%;
-    height: 100%;
-    margin: 0%;
-    padding: 0%;
-}
-  
-}
-@media screen and (max-width:1400px) and (min-width: 771px) {
-  .home{
-    width: 100%;
-    height: calc(100vh - 40px);
-    color: white;
-    margin-left: 10%;
-    margin-right: 1px;
-}
-}
-</style>
+<style src="./Works.css" scoped></style>
