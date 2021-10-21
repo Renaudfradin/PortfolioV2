2cfd63e2947f8acb60f96323d6e23b4c015a269a
Renaudfradin <renaudfradin@gmail.com>
2021-10-21 23:54:05 +0200

    work update data

--- src/components/Work/Work.vue
+++ src/components/Work/Work.vue
@@ -1,37 +1,33 @@
 <template>
-    <div>
-        <div v-for="(projet , index) in tableproj" v-bind:key="index">  
-            <div v-if="projet.nomProjet == $route.params.name">
-                <p style="margin-left: 200px;">nom : {{projet.nomProjet}}</p>
-                <p style="margin-left: 200px;">description : {{projet.diescriptionProjet}}</p>
-                <p style="margin-left: 200px;">quand : {{projet.realisationProjet}}</p>
-                <p style="margin-left: 200px;">img projet : {{projet.imgProjet}}</p>
-                <p style="margin-left: 200px;">code proejet :  {{projet.urlCodeProjet}}</p>
-                <p style="margin-left: 200px;">siet proejet : {{projet.urlSiteProjet}}</p>
-            </div> 
-
-        </div> 
-    </div>
+  <div>
+    <div v-for="(projet , index) in jsonData" v-bind:key="index"> 
+      <div v-for="(projetss , index2) in projet" v-bind:key="index2">
+        <div v-if="projetss.nomProjet == $route.params.name">
+            <p style="margin-left: 200px;">nom : {{projetss.nomProjet}}</p>
+            <p style="margin-left: 200px;">description : {{projetss.diescriptionProjet}}</p>
+            <p style="margin-left: 200px;">quand : {{projetss.realisationProjet}}</p>
+            <p style="margin-left: 200px;">img projet : {{projetss.imgProjet}}</p>
+            <p style="margin-left: 200px;">code proejet :  {{projetss.urlCodeProjet}}</p>
+            <p style="margin-left: 200px;">site proejet : {{projetss.urlSiteProjet}}</p>
+            <li v-for="(stack , index3) in projetss.stackProjet" v-bind:key="index3">
+              <p style="margin-left: 200px;">{{stack.stack1}}</p>
+              <p style="margin-left: 200px;">{{stack.stack2}}</p>
+              <p style="margin-left: 200px;">{{stack.stack3}}</p>
+              <p style="margin-left: 200px;">{{stack.stack4}}</p>
+              <p style="margin-left: 200px;">{{stack.stack5}}</p>
+            </li>
+        </div>
+      </div>
+    </div> 
+  </div>
 </template>
 <script>
 import { defineComponent } from 'vue';
+import json from '../jsondata/data.json';
 export default defineComponent({
   data(){
     return{
-      tableproj:[
-        {nomProjet:"SAOARS-DB",diescriptionProjet:"Site regroupant tous les personnages du jeu mobile Sword Art Online Alicization Rising Steel. Le site fonctionne avec l'api API-SAOARS.",realisationProjet:"AVRIL 2021 - en cour ",imgProjet:"imgwork/saoarsdb.png",urlCodeProjet:"https://github.com/Renaudfradin/SAOARS-DBV1",urlSiteProjet:"https://saoars-db.netlify.app"},
-        {nomProjet:"API-SAOARS",diescriptionProjet:"API qui regroupe les personnages du jeu mobile Sword Art Online Alicization Rising Steel. Réalisée en NODE.JS/EXPRESS.",realisationProjet:"JANVIER 2021 - en cour ",imgProjet:"imgwork/apisaoars.png",urlCodeProjet:"https://github.com/Renaudfradin/API-SAOARS",urlSiteProjet:"https://api-saoars.herokuapp.com/"},
-        {nomProjet:"NUMBER 10",diescriptionProjet:"Projet de cours réalisé en WORDPRESS.",realisationProjet:"Mars 2021 - Mars 2021",imgProjet:"imgwork/number10.png",urlCodeProjet:"rien",urlSiteProjet:"rien"},
-        {nomProjet:"EIGA",diescriptionProjet:"Projet de cours utilisant l'api IMDB. Réalisé en REACT.",realisationProjet:"Novembre 2020 - Novembre 2020",imgProjet:"imgwork/portfoliov2.png",urlCodeProjet:"https://github.com/anthony-roux/Movie-Search-Engine_tp",urlSiteProjet:"https://eiga.netlify.app/"},
-        {nomProjet:"PROTFOLIO V2",diescriptionProjet:"Portfolio qui regroupe mes compétences. Réalisé en VUE.JS.",realisationProjet:"Octobre 2020",imgProjet:"imgwork/portfoliov2.png",urlCodeProjet:"https://github.com/Renaudfradin/PortfolioV2",urlSiteProjet:"https://portfolio-renaud-fradin.netlify.app"},
-        {nomProjet:"PROTFOLIO V1",diescriptionProjet:"Portfolio qui regroupe mes compétences. Réalisé en BULMA/JS.",realisationProjet:"Décembre 2019 - Septembre 2020",imgProjet:"imgwork/portfoliov1.png",urlCodeProjet:"https://github.com/Renaudfradin/Portfolio",urlSiteProjet:"https://portfolio-renaud-fradinv1.netlify.app"},
-        {nomProjet:"The Skateshop",diescriptionProjet:"Site vitrine de vente de skate. Réalisé en HTML/CSS/JS.",realisationProjet:"Avril 2020",imgProjet:"imgwork/TheSkateshop.png",urlCodeProjet:"https://github.com/Renaudfradin/The-Skateshop",urlSiteProjet:"https://renaudfradin.github.io/The-Skateshop/"},
-        {nomProjet:"BOT DISCORD(MaXiMe)",diescriptionProjet:"BOT réalisé pour le logiciel Discord.",realisationProjet:"Mai 2020",imgProjet:"imgwork/Botdiscord.png",urlCodeProjet:"https://github.com/Renaudfradin/BOT-MAXIME-DISCORD",urlSiteProjet:"rien"},
-        {nomProjet:"GSB-PHP",diescriptionProjet:"Application de gestion de frais. Réalisée en BULMA/PHP/SQL.",realisationProjet:"Mars 2020 - Juin 2020",imgProjet:"imgwork/gsb.png",urlCodeProjet:"https://github.com/Renaudfradin/GSB-PHP",urlSiteProjet:"rien"},
-        {nomProjet:"GSB-JAVA",diescriptionProjet:"Application de gestion de compte-rendu de visite. Réalisée en JAVA.",realisationProjet:"Février 2020 - Mai 2020",imgProjet:"imgwork/GSB-JAVA.png",urlCodeProjet:"https://github.com/Renaudfradin/GSB-JAVA",urlSiteProjet:"rien"},
-        {nomProjet:"EDITCALC",diescriptionProjet:"Calculette pour enfant. Réalisée en JAVA/SQL.",realisationProjet:"Novembre 2019 - Avril 2020",imgProjet:"imgwork/EDITCALC.png",urlCodeProjet:"https://github.com/Renaudfradin/EDITCAL-JAVA",urlSiteProjet:"rien"},
-        {nomProjet:"Location-Cars-V1",diescriptionProjet:"Site de locations de voitures (ﬁctif). Réalisé en HTML/CSS/JS/PHP/SQL.",realisationProjet:"Avril 2019 - Juin 2019",imgProjet:"imgwork/locationscars.png",urlCodeProjet:"https://github.com/Renaudfradin/location-car-v1/tree/master/location-cars",urlSiteProjet:"rien"},
-      ]
+      jsonData:json
     }
   }
 })
--- src/components/Works/Works.vue
+++ src/components/Works/Works.vue
@@ -1,18 +1,24 @@
 <template>
 <div class="home">
-  <div class="row">
-    <div v-for="(projet , index) in tableproj" v-bind:key="index" class="col" >   
+  <div v-for="(projet , index) in jsonData" v-bind:key="index" class="row">
+    <div v-for="(projetss , index2) in projet" v-bind:key="index2" class="col" >  
       <div class="style_card">
-        <router-link :to="{ name: 'Work' , params: { name:projet.nomProjet }}"><img class="card_img" :src="projet.imgProjet" alt=""></router-link>
+        <router-link :to="{ name: 'Work' , params: { name:projetss.nomProjet }}"><img class="card_img" :src="projetss.imgProjet" alt=""></router-link>
         <div class="cardbody">
           <div class="text_info">
-            <h5 class="text_info_name">{{projet.nomProjet}}</h5>
-            <p class="text_info_description">{{projet.diescriptionProjet}}</p>
-            <p class="text_info_btnprojet">{{projet.realisationProjet}}</p>
+            <h5 class="text_info_name">{{projetss.nomProjet}}</h5>
+            <p class="text_info_description">{{projetss.diescriptionProjet}}</p>
+            <div class="stack_info">
+              <p v-for="(stack , index3) in projetss.stackProjet" v-bind:key="index3" class="stack_style" >
+                {{stack.stack1}}  {{stack.stack2}} {{stack.stack3}} {{stack.stack4}} {{stack.stack5}}
+              </p>
+            </div>
+              
+            <p class="text_info_btnprojet">{{projetss.realisationProjet}}</p>
           </div>
           <div class="btn_parent">
-            <a v-if="projet.urlSiteProjet != 'rien'" :href="projet.urlSiteProjet" class="btn_info" target="_blank">site</a>
-            <a v-if="projet.urlCodeProjet != 'rien'" :href="projet.urlCodeProjet" class="btn_info" target="_blank">code</a>
+            <a v-if="projetss.urlSiteProjet != 'rien'" :href="projetss.urlSiteProjet" class="btn_info" target="_blank">site</a>
+            <a v-if="projetss.urlCodeProjet != 'rien'" :href="projetss.urlCodeProjet" class="btn_info" target="_blank">code</a>
           </div>
         </div>
       </div>
@@ -22,23 +28,11 @@
 </template>
 <script>
 import { defineComponent } from 'vue';
+import json from '../jsondata/data.json';
 export default defineComponent({
   data(){
     return{
-      tableproj:[
-        {nomProjet:"SAOARS-DB",diescriptionProjet:"Site regroupant tous les personnages du jeu mobile Sword Art Online Alicization Rising Steel. Le site fonctionne avec l'api API-SAOARS.",realisationProjet:"AVRIL 2021 - en cour ",imgProjet:"imgwork/saoarsdb.png",urlCodeProjet:"https://github.com/Renaudfradin/SAOARS-DBV1",urlSiteProjet:"https://saoars-db.netlify.app"},
-        {nomProjet:"API-SAOARS",diescriptionProjet:"API qui regroupe les personnages du jeu mobile Sword Art Online Alicization Rising Steel. Réalisée en NODE.JS/EXPRESS.",realisationProjet:"JANVIER 2021 - en cour ",imgProjet:"imgwork/apisaoars.png",urlCodeProjet:"https://github.com/Renaudfradin/API-SAOARS",urlSiteProjet:"https://api-saoars.herokuapp.com/"},
-        {nomProjet:"NUMBER 10",diescriptionProjet:"Projet de cours réalisé en WORDPRESS.",realisationProjet:"Mars 2021 - Mars 2021",imgProjet:"imgwork/number10.png",urlCodeProjet:"rien",urlSiteProjet:"rien"},
-        {nomProjet:"EIGA",diescriptionProjet:"Projet de cours utilisant l'api IMDB. Réalisé en REACT.",realisationProjet:"Novembre 2020 - Novembre 2020",imgProjet:"imgwork/portfoliov2.png",urlCodeProjet:"https://github.com/anthony-roux/Movie-Search-Engine_tp",urlSiteProjet:"https://eiga.netlify.app/"},
-        {nomProjet:"PROTFOLIO V2",diescriptionProjet:"Portfolio qui regroupe mes compétences. Réalisé en VUE.JS.",realisationProjet:"Octobre 2020",imgProjet:"imgwork/portfoliov2.png",urlCodeProjet:"https://github.com/Renaudfradin/PortfolioV2",urlSiteProjet:"https://portfolio-renaud-fradin.netlify.app"},
-        {nomProjet:"PROTFOLIO V1",diescriptionProjet:"Portfolio qui regroupe mes compétences. Réalisé en BULMA/JS.",realisationProjet:"Décembre 2019 - Septembre 2020",imgProjet:"imgwork/portfoliov1.png",urlCodeProjet:"https://github.com/Renaudfradin/Portfolio",urlSiteProjet:"https://portfolio-renaud-fradinv1.netlify.app"},
-        {nomProjet:"The Skateshop",diescriptionProjet:"Site vitrine de vente de skate. Réalisé en HTML/CSS/JS.",realisationProjet:"Avril 2020",imgProjet:"imgwork/TheSkateshop.png",urlCodeProjet:"https://github.com/Renaudfradin/The-Skateshop",urlSiteProjet:"https://renaudfradin.github.io/The-Skateshop/"},
-        {nomProjet:"BOT DISCORD(MaXiMe)",diescriptionProjet:"BOT réalisé pour le logiciel Discord.",realisationProjet:"Mai 2020",imgProjet:"imgwork/Botdiscord.png",urlCodeProjet:"https://github.com/Renaudfradin/BOT-MAXIME-DISCORD",urlSiteProjet:"rien"},
-        {nomProjet:"GSB-PHP",diescriptionProjet:"Application de gestion de frais. Réalisée en BULMA/PHP/SQL.",realisationProjet:"Mars 2020 - Juin 2020",imgProjet:"imgwork/gsb.png",urlCodeProjet:"https://github.com/Renaudfradin/GSB-PHP",urlSiteProjet:"rien"},
-        {nomProjet:"GSB-JAVA",diescriptionProjet:"Application de gestion de compte-rendu de visite. Réalisée en JAVA.",realisationProjet:"Février 2020 - Mai 2020",imgProjet:"imgwork/GSB-JAVA.png",urlCodeProjet:"https://github.com/Renaudfradin/GSB-JAVA",urlSiteProjet:"rien"},
-        {nomProjet:"EDITCALC",diescriptionProjet:"Calculette pour enfant. Réalisée en JAVA/SQL.",realisationProjet:"Novembre 2019 - Avril 2020",imgProjet:"imgwork/EDITCALC.png",urlCodeProjet:"https://github.com/Renaudfradin/EDITCAL-JAVA",urlSiteProjet:"rien"},
-        {nomProjet:"Location-Cars-V1",diescriptionProjet:"Site de locations de voitures (ﬁctif). Réalisé en HTML/CSS/JS/PHP/SQL.",realisationProjet:"Avril 2019 - Juin 2019",imgProjet:"imgwork/locationscars.png",urlCodeProjet:"https://github.com/Renaudfradin/location-car-v1/tree/master/location-cars",urlSiteProjet:"rien"},
-      ]
+      jsonData:json
     }
   }
 })
