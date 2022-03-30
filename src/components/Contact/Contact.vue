e38425531cb50befa8ac2a078fd219a830212ecb
Renaudfradin <renaudfradin@gmail.com>
2022-03-30 23:47:14 +0200

    update text

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -2,10 +2,10 @@
 <div class="presentation">
   <div class="presentation2">
     <h1 class="skills">Contactez moi</h1>
-    <p class="metier">Je suis interessé par les grands ou petits projets.</p>
+    <p class="metier">Je suis interessé par les grands ou petits projets : il n'y a que la création qui compte !</p>
     <p class="metier">Si vous avez des questions, n'hésitez pas à me contacter en utilisant mon adresse mail ou mon linkedin :</p>
     <p class="metier">renaudfradin@gmail.com</p>
-    <p class="metier"><a href="https://www.linkedin.com/in/Renaudfradin/" onclick="window.open(this.href); return false;">Linkedin Renaud fradin</a></p>
+    <p class="metier"><a href="https://www.linkedin.com/in/Renaudfradin/" target="_blank">Linkedin Renaud fradin</a></p>
   </div>
 </div>
 </template>
