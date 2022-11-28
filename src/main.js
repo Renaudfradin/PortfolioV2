a1fa5a8bdca6011c3a07fa746a50b60876fe1212
Renaud <renaudfradin@gmail.com>
2022-11-28 22:52:57 +0100

    add meta tags

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createMetaManager } from 'vue-meta'
 
 const app = createApp(App)
-
-app.use(router)
+  .use(router)
+  .use(createMetaManager ())
 
 app.mount('#app')
