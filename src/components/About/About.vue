61104c35aa93bb9692ab92ae01b4e5015e8c56ab
Renaud <renaudfradin@gmail.com>
2022-12-20 00:18:03 +0100

    update : corection date and project data

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -2,22 +2,31 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  setup() { },
+  data(){
+    return{
+      dateResult: null
+    }
+  },
+  created(){
+    const dateDirthDay = new Date('Januery 17, 2000 14:00:00').getFullYear()
+    const acuelleDate = new Date().getFullYear();
+    this.dateResult = acuelleDate - dateDirthDay;
+  }
 })
 </script>
 
 <template>
   <div class="presentationCard">
     <h3 class="presentationtitle">Présentation</h3>
-    <p>Je suis un étudiant de 22 ans.</p>
+    <p>Je suis un étudiant de {{dateResult}} ans.</p>
     <p>Je suis actuellement en Mastère CTO & Tech Lead a l'école Eemi.</p>
-    <p>J'ai obtenu un Bachelor développeur web à Hetic</p>
+    <p>J'ai obtenu un Bachelor développeur web à Hetic en septembre 2022</p>
     <p>J'ai obtenu un BTS SIO (Service Informatique aux Organisations) SLAM (Solutions Logicielles et Applications Métiers) en juin 2020.</p>
     <p>J'ai obtenu un Titre professionnel Développeur web et web mobile à L'AFPA en juin 2019.</p>
     <p>J'ai obtenu mon baccalauréat STI2D (sciences & technologies de l'industrie et du développement durable) en juin 2018.</p>
-    <p>Pendant mon temps libre je travaille sur des projet personelle , je fait du sport (c’est une passion et une motivation) , j'étudie aussi l’histoire et je joue aussi aux jeux vidéo (fps , rpg).</p>
+    <p>Pendant mon temps libre je travaille sur des projet personelle , je fait du sport (c’est une passion et une motivation) , j'étudie aussi l’histoire et je joue aussi aux jeux vidéo (fps , rpg, mmo).</p>
     <p>Je milite activement pour la cause animale.</p>
-    <a href="cv/cv.pdf" download="cv.pdf" class="btnCV"> voir mon cv</a>
+    <a href="cv/cv_renaud.pdf" download="CV_Renaud.pdf" class="btnCV">voir mon cv</a>
   </div>
 </template>
 
