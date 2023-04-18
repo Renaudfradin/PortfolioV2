4a3e7b8885537f1cb9c26e9176e0414179c37b88
Renaud <renaudfradin@gmail.com>
2023-04-18 19:21:40 +0200

    move on next js

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { createMetaManager } from 'vue-meta'
-
-const app = createApp(App)
-  .use(router)
-  .use(createMetaManager ())
-app.mount('#app')
