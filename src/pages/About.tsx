b757e5e611fee9b54bd0ea96d617f5259ff2905f
Renaud <renaudfradin@gmail.com>
2023-05-20 14:12:18 +0200

    update : switch js to ts

---
+++ src/pages/About.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About page</title>
+        <meta name="description" content="Aboutpage portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <p>About page</p>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/Works.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Works() {
+  return (
+    <>
+      <Head>
+        <title>Works page</title>
+        <meta name="description" content="Workspage portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <p>Works page</p>
+      </div>
+    </>
+  )
+}
