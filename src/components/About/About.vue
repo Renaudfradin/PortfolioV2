8bdd0f3315bacbc44d4f52ebce9015c28e211e9c
Renaud <renaudfradin@gmail.com>
2024-02-25 12:54:45 +0100

    update data and version

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -13,13 +13,13 @@ export default defineComponent({
 <template>
   <div class="presentationCard">
     <h3 class="presentationtitle">Présentation</h3>
-    <p>Je suis un étudiant de 23 ans.</p>
+    <p>Je suis un étudiant de 24 ans.</p>
     <p>Je suis actuellement en Mastère CTO & Tech Lead a l'école EEMI.</p>
     <p>J'ai obtenu un Bachelor développeur web à Hetic en septembre 2022.</p>
     <p>J'ai obtenu un BTS SIO (Service Informatique aux Organisations) SLAM (Solutions Logicielles et Applications Métiers) en juin 2020.</p>
     <p>J'ai obtenu un Titre professionnel Développeur web et web mobile à L'AFPA en juin 2019.</p>
     <p>J'ai obtenu mon baccalauréat STI2D (sciences & technologies de l'industrie et du développement durable) en juin 2018.</p>
-    <p>Pendant mon temps libre je travaille sur des projet personelle , je fait du sport (c’est une passion et une motivation) , j'étudie aussi l’histoire et je joue aussi aux jeux vidéo (fps , rpg, mmo).</p>
+    <p>Pendant mon temps libre je travaille sur des projet personelle , je fait du sport (c’est une passion et une motivation) , j'étudie aussi l’histoire et je joue aussi aux jeux vidéo.</p>
     <p>Je milite activement pour la cause animale.</p>
     <a href="cv/cv_renaud.pdf" download="CV_Renaud.pdf" class="btnCV">voir mon cv</a>
   </div>
